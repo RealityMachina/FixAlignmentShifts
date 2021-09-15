bf4cb08c6956b1f9443e4ff26a1cb4140d9f4235
RealityMachina <tomclancy1995@gmail.com>
2021-09-15 10:56:02 -0400

    Initial release.

---
+++ FixAlignmentShifts/AlignmentChecker.cs
@@ -0,0 +1,58 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.Alignments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FixAlignmentShifts
+{
+    [HarmonyPatch(typeof(UnitAlignment), "Shift",  new Type[] { typeof(AlignmentShiftDirection), typeof(int), typeof(IAlignmentShiftProvider) })]
+    class AlignmentChecker
+    {
+
+        private static void Prefix(UnitAlignment __instance, ref AlignmentShiftDirection direction, int value)
+        {
+
+            Main.Log("Get current alignment direction " + direction.ToString());
+            Main.Log("Get our current alignment " + __instance.m_Value.ToString());
+
+            if(direction == AlignmentShiftDirection.Evil)
+            {
+                switch (__instance.m_Value)
+                {
+                    case Alignment.LawfulEvil:
+                        direction = AlignmentShiftDirection.LawfulEvil;
+                        break;
+                    case Alignment.ChaoticEvil:
+                        direction = AlignmentShiftDirection.ChaoticEvil;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            else if (direction == AlignmentShiftDirection.Good)
+            {
+                switch (__instance.m_Value)
+                {
+                    case Alignment.ChaoticGood:
+                        direction = AlignmentShiftDirection.ChaoticGood;
+                        break;
+                    case Alignment.LawfulGood:
+                        direction = AlignmentShiftDirection.LawfulGood;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Main.Log("Get current alignment direction " + direction.ToString());
+            return;
+        }
+
+    }
+}
---
+++ FixAlignmentShifts/Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+using Kingmaker.Blueprints.JsonSystem;
+//using Kingmaker.Blueprints.JsonSystem;
+
+using UnityEngine;
+using UnityModManagerNet;
+using Kingmaker.Localization;
+using static UnityModManagerNet.UnityModManager;
+
+namespace FixAlignmentShifts
+{
+    public class Main
+    {
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+            var harmony = new Harmony(modEntry.Info.Id);
+
+            savedModEntry = modEntry;
+           // ModSettings.LoadAllSettings();
+            //modEntry.OnGUI = new Action<UnityModManager.ModEntry>(Main.OnGUI);
+            //   modEntry.OnSaveGUI = new Action<UnityModManager.ModEntry>(Main.OnSaveGUI);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+           // PostPatchInitializer.Initialize();
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Main.iAmEnabled = value;
+            return true;
+        }
+
+        public static void Log(string msg)
+        {
+            savedModEntry.Logger.Log(msg);
+        }
+
+        private static bool iAmEnabled;
+
+        public static ModEntry savedModEntry;
+    }
+}
